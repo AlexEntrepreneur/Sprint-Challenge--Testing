c0eb2b1493da96ee114823ee8d0f8406d4e1169d
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-04-12 10:50:00 +0100

    Stretch: Add ID Generation

--- routers/gamesRouter.js
+++ routers/gamesRouter.js
@@ -1,41 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
+let gamesData = [
+  {
+    id: 1,
+    title: 'Pacman',
+    genre: 'Arcade',
+    releaseYear: 1980
+  },
+  {
+    id: 2,
+    title: 'Crysis',
+    genre: 'First-person Shooter',
+    releaseYear: 2007
+  }
+];
+
 router.use(express.json());
 
 router.get('/', (req, res) => {
-  res.json([
-    {
-      title: 'Pacman',
-      genre: 'Arcade',
-      releaseYear: 1980
-    },
-    {
-      title: 'Crysis',
-      genre: 'First-person Shooter',
-      releaseYear: 2007
-    }
-  ]);
+  res.json(gamesData);
 });
 
 router.post('/', (req, res) => {
-  const newGame = req.body;
+  const newGame = { id: ++gamesData.length, ...req.body};
   const { title, genre, releaseYear } = newGame;
 
   if (title && genre) {
-    res.json([
-      {
-        title: 'Pacman',
-        genre: 'Arcade',
-        releaseYear: 1980
-      },
-      {
-        title: 'Crysis',
-        genre: 'First-person Shooter',
-        releaseYear: 2007
-      },
-      newGame
-    ])
+    res.json([...gamesData, newGame]);
   }
   res.status(422).json({
     message: 'A title and genre field are required. Please try again.'
